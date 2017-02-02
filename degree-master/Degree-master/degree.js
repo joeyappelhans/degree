e29b726b2194984134c7d4e57ef2854f96b1b549
joeyappelhans <9jappelhans@stpatrick.org>
2017-02-02 13:51:22 -0600

    finished degree centrality

---
+++ degree-master/Degree-master/degree.js
@@ -0,0 +1,29 @@
+const Matrix = require("matrix-js");
+
+let A = Matrix([
+    [1,1,0,1,0],
+    [1,0,1,0,0],
+    [0,1,0,0,0],
+    [1,0,0,0,1],
+    [0,0,0,1,0]
+]);
+//Code here
+function degree(mtrx,v){
+  var sum=0;
+  var degreelist= mtrx(v);
+  for(let e=0;e<degreelist.length;e++){
+    sum=sum+degreelist[e];
+  }
+  return sum
+}
+
+function degreecentrality(mtrx,v){
+  var neighbors= mtrx.size;
+  return degree(mtrx,v)/neighbors;
+}
+
+//Challenge
+
+//Run test code with ctrl+shift+p  then type in "script"
+console.log(degree(A,3));
+console.log(degreecentrality(A,2));
